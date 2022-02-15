9458b2ae18662338dff3e7178f00ecb66d63a208
Tsvetislava Yankova <tsvetislava.yankova@gmail.com>
2022-02-15 12:12:14 +0200

    Dependency update

--- src/main/java/com/example/ittalentsfinalprojectairbnb/ItTalentsFinalProjectAirbnbApplication.java
+++ src/main/java/com/example/ittalentsfinalprojectairbnb/ItTalentsFinalProjectAirbnbApplication.java
@@ -1,7 +1,11 @@
 package com.example.ittalentsfinalprojectairbnb;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ItTalentsFinalProjectAirbnbApplication {
@@ -10,4 +14,13 @@ public class ItTalentsFinalProjectAirbnbApplication {
         SpringApplication.run(ItTalentsFinalProjectAirbnbApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
