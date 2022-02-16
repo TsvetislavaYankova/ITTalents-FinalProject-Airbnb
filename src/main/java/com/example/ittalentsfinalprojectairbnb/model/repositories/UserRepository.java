02ba068c051fb82c209a0e27a37bce766aedd037
Tsvetislava Yankova <tsvetislava.yankova@gmail.com>
2022-02-16 22:42:49 +0200

    user register updated checks

--- src/main/java/com/example/ittalentsfinalprojectairbnb/model/repositories/UserRepository.java
+++ src/main/java/com/example/ittalentsfinalprojectairbnb/model/repositories/UserRepository.java
@@ -9,4 +9,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByEmailAndPassword(String email, String password);
     User findByEmail(String email);
+    User findByPhoneNumber(String phoneNumber);
 }
