bfa42e88c79a924b43371e684c23565e631ffbac
Tsvetislava Yankova <tsvetislava.yankova@gmail.com>
2022-02-16 20:01:47 +0200

    entity added

--- src/main/java/com/example/ittalentsfinalprojectairbnb/model/entities/PropertyPhoto.java
+++ src/main/java/com/example/ittalentsfinalprojectairbnb/model/entities/PropertyPhoto.java
@@ -1,6 +1,23 @@
 package com.example.ittalentsfinalprojectairbnb.model.entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "properties_photos")
+@Getter
+@Setter
+@NoArgsConstructor
 public class PropertyPhoto {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String photo_url;
+    @Column
+    private int property_id;
 }
--- src/main/java/com/example/ittalentsfinalprojectairbnb/model/entities/User.java
+++ src/main/java/com/example/ittalentsfinalprojectairbnb/model/entities/User.java
@@ -1,6 +1,6 @@
 package com.example.ittalentsfinalprojectairbnb.model.entities;
 
-import lombok.Data;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
